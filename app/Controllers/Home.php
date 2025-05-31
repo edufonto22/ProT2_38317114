8ca2126d9cd65194101da10b0e8abd62806cb47e
edufonto22 <eduardofontora7@gmail.com>
2025-05-30 23:39:43 -0300

    se hizo el ruteo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,30 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('front/principal');
+        echo view('front/head_view');
+        echo view('front/navbar_view');
+        echo view('front/principal_ultimo');
+        echo view('front/footer_view');
     }
+
+    public function quienes_somos()
+    {
+        echo view('front/head_view');
+        echo view('front/navbar_view');
+        echo view('front/principal_ultimo');
+        echo view('front/footer_view');
+    }
+
+    public function acerca_de()
+    {
+        echo view('front/head_view');
+        echo view('front/navbar_view');
+        echo view('front/principal_ultimo');
+        echo view('front/footer_view');
+    }
+
+
+
 }
